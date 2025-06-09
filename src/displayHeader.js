0479847c8ac24c1316126c0fc105f6ceacd7ca6c
19seniman <161638164+19seniman@users.noreply.github.com>
2025-06-10 01:33:11 +0700

    displayHeader.js

--- src/displayHeader.js
+++ src/displayHeader.js
@@ -3,9 +3,9 @@ const colors = require("colors");
 function displayHeader() {
   process.stdout.write("\x1Bc");
   console.log(colors.cyan("========================================"));
-  console.log(colors.cyan("=        EVM auto Tx2                  ="));
-  console.log(colors.cyan("=     Author: HCA & !9Seniman          ="));
-  console.log(colors.cyan("=   🍉🍉 FREE PALESTINE 🍉🍉         ="));
+  console.log(colors.cyan("=        EVM auto Tx2                   ="));
+  console.log(colors.cyan("=     Author: HCA & !9Seniman           ="));
+  console.log(colors.cyan("=   🍉🍉 FREE PALESTINE 🍉🍉          ="));
   console.log(colors.cyan("========================================"));
   console.log();
 }
